194c4fbe69fa8c83fdff2a82304ade5720fe3e9d
Sudharshan <sudhar393@gmail.com>
2018-06-24 02:22:09 +0800

    ports

--- vocals_server/app.js
+++ vocals_server/app.js
@@ -14,7 +14,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('port', process.env.PORT || 40025);
 
 app.use(logger('dev'));
 app.use(express.json());
