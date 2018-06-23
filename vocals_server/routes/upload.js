ff0cc4ab80f06709f353eb06004e5ab3e8cb9a32
Sudharshan <sudhar393@gmail.com>
2018-06-23 22:49:26 +0800

    super vulnerable everything

--- vocals_server/routes/upload.js
+++ vocals_server/routes/upload.js
@@ -1,23 +1,30 @@
 var express = require('express');
 var fileUpload = require('express-fileupload');
 var path = require('path');
+var fs = require("fs");
 
 var router = express.Router();
 
-router.post('/', function(req, res) {
+router.post('/client', function(req, res) {
     if (!req.files)
-        return res.status(400).send('No files were uploaded.');
+        return res.status(400).send('No files were uploaded.(client)');
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let sampleFile = req.files.sampleAudio;
 
     // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv((path.resolve('public/audio/audio_sample.jpg')), function(err) {
+    sampleFile.mv((path.resolve('public/audio/audio_sample.wav')), function(err) {
     if (err)
         return res.status(500).send(err);
 
     res.sendFile(path.resolve('public/complete.html'));
     });
+});
+
+router.post('/process', function(req, res){
+    var outStream = fs.createWriteStream(path.resolve('public/audio/audio_output.wav'));
+    req.pipe(outStream);
+    res.send('Success');
 });
 
 module.exports = router;
